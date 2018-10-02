1d029624e025f7f1a2e74d357c8956f14cf68a61
Genna Walsh <gennawalsh@gmail.com>
2018-10-02 20:53:01 +0100

    erb, welcome and stylesheet

--- controller.rb
+++ controller.rb
@@ -5,9 +5,13 @@ require("pry")
 require_relative("./models/game.rb")
 also_reload("./models/*")
 
-get("/:player1/:player2") do
-  play1 = params[:player1]
-  play2 = params[:player2]
-  @result = Game.new(play1, play2)
-  erb(:result)
+get("/") do
+  erb(:welcome)
+end
+
+get("/:play1/:play2") do
+  play1 = params[:play1]
+  play2 = params[:play2]
+  @winner = Game.play(play1, play2)
+  erb(:winner)
 end
--- models/game.rb
+++ models/game.rb
@@ -3,33 +3,34 @@ class Game
   def self.play(play1, play2)
 
     if play1 == "rock" && play2 == "paper"
-      return "Player 2 wins!"
+      result = "Player 2 wins with Paper!"
     # rock / paper
     elsif play1 == "rock" && play2 == "scissors"
-        return "Player 1 wins!"
+      result = "Player 1 wins with Rock!"
     # rock / scissors
     elsif play1 == "rock" && play2 == "rock"
-        return "It's a draw!"
+      result = "It's a draw! You both chose Rock."
     # rock / rock
     elsif play1 == "paper" && play2 == "rock"
-        return "Player 1 wins!"
+      result = "Player 1 wins with Paper!"
     # paper / rock
     elsif play1 == "paper" && play2 == "paper"
-        return "It's a draw!"
+      result = "It's a draw! You both chose Paper."
     # paper /paper
     elsif play1 == "paper" && play2 == "scissors"
-        return "Player 1 wins!"
+      result = "Player 1 wins with Paper!"
     # paper / scissors
     elsif play1 == "scissors" && play2 == "rock"
-        return "Player 2 wins!"
+      result = "Player 2 wins with Rock!"
     # scissors / rock
     elsif play1 == "scissors" && play2 == "paper"
-        return "Player 1 wins!"
+      result = "Player 1 wins with Scissors!"
     # scissors / paper
     else play1 == "scissors" && play2 == "scissors"
-        return "It's a draw!"
+      result = "It's a draw! You both chose Scissors."
     # scissors / scissors
     end
+    return result
 
   end
 
